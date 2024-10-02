631dbd04fe5041b138623d98cbd0020d52861dd6
muhammadridho4 <ridhomalang066@gmail.com>
2024-10-02 11:09:30 +0700

    add

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-docker build -t karsajobs.ui:latest .
-docker images
-docker karsajobs.ui:latest  ghcr.io/muhammadridho4/karsajobs.ui:latest
-echo $GITHUB_TOKEN | docker login ghcr.io -u muhammadridho4 --password-stdin
-docker push ghcr.io/muhammadridho4/karsajobs.ui:latest
+docker build -t ghcr.io/muhammadridho4/karsajobs_ui:latest .
+echo $CR_PAT | docker login ghcr.io -u muhammadridho4 --password-stdin
+docker push ghcr.io/muhammadridho4/karsajobs_ui:latest
